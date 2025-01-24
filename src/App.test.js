ad7a5a5705468c7468f17f02fdd8028bda4b480c
Bhavana104 <bhavanasawarkar06@gmail.com>
2025-01-23 19:34:08 -0500

    Test updated

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,7 @@ import App from './App';
 import '@testing-library/jest-dom';
 
 test('renders learn react link', () => {
- 
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
